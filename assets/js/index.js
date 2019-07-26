348372cace2f44e2998afea7515a8f1ff05023d0
Haotian Liang <m13621206279@hotmail.com>
2019-07-26 20:58:03 +0800

    modify

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,12 @@ $('.tab .menus li').each(function(){
                 'assets/bg1.png',
                 'assets/bg2.png',
                 'assets/bg3.png',
+                'assets/bg4.png',
+                'assets/bg5.png',
+                'assets/bg6.png',
+                'assets/bg7.png',
+            ] // 背景图片  不需要注释即可
         $('.tab .scroll').css('margin-top',-index*800+'px');
-        $('.tab').css('background-image','url('+arr[index]+')')
+        $('.tab').css('background-image','url('+arr[index]+')')// 背景图片  不需要注释即可
     })
 })
